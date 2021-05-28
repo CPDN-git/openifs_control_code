33d16ec28c0489f324c6f1f005353822832dd94d
Andy Bowery <abowery@users.noreply.github.com>
2021-05-28 15:12:55 +0100

    Removing redundant code

--- openifs.cpp
+++ openifs.cpp
@@ -25,18 +25,6 @@
 #include <signal.h>
 #include <fcntl.h>
 
-//#ifndef __has_include
-//  static_assert(false, "__has_include not supported");
-//#else
-//#  if __has_include(<filesystem>)
-//#    include <filesystem>
-//     namespace fs = std::filesystem;
-//#  elif __has_include(<experimental/filesystem>)
-//#    include <experimental/filesystem>
-//     namespace fs = std::experimental::filesystem;
-//#  endif
-//#endif
-
 #ifndef _MAX_PATH
    #define _MAX_PATH 512
 #endif
